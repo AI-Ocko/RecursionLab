d7dff07cd787b0da7fb26931072254eff2ff4613
christopher <135375609+AI-Ocko@users.noreply.github.com>
2025-04-07 22:12:14 -0700

    Created writeBackward function

--- RecursionLab.cpp
+++ RecursionLab.cpp
@@ -3,7 +3,32 @@
 
 using namespace std;
 
+void writeBackward(string userInputPar);
+
 int main () {
 
+    //declarations
+
+    //get user input
+
+    //calculations
+
+    //output
+
     return 0;
+}
+
+//version 1 (strip away the last character approach)
+void writeBackward(string userInputPar) {
+    int lenOfString = userInputPar.length();
+
+    // 0 1 2
+    // T O M
+
+    if (lenOfString > 0) {
+        cout << userInputPar.substr(lenOfString -1, 1); // display the last character
+        writeBackward(userInputPar.substr(0, lenOfString - 1));
+
+    }
+    //else if the lenOfString is 0, then we do nothing, implying a base case
 }
